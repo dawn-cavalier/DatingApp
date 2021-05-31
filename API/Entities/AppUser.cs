71dc9d271b6fad3a61069626df457b6ccea1792a
Tyler Toper <tyler.toper@gmail.com>
2021-05-31 15:57:47 -0700

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        // Both are using names that are part of coventions
+        // Change at your own peril
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        
+    }
+}
