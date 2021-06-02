c2fb6e2e5c8c6a411400f1d5ff9f2a7b37d980cf
ttoper <tyler.toper@gmail.com>
2021-06-02 11:12:15 -0700

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,6 +6,7 @@ namespace API.Entities
         // Change at your own peril
         public int Id { get; set; }
         public string UserName { get; set; }
-        
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
